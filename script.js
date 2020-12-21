01a12122d44e36f500b24762e8fa47ff237be27a
Leonardo <leonardo.luize2@gmail.com>
2020-12-21 19:39:55 -0300

    operation in desktop console

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World! :)");
+var args = process.argv.slice(2)
+
+var a = Number(args[1]);
+var b = Number(args[2]);
+var c = "";
+
+function add(x, y){
+    return x + y;
+}
+
+function mult(x, y){
+    return x * y;
+}
+
+if(args[0] == "a"){
+    c = add(a, b);
+}else if(args[0] == "m"){
+    c = mult(a, b);
+}
+
+console.log(c)
